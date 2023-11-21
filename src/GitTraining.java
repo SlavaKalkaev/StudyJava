0277bed147228fe25d244f577b7718dd2db40c40
Вячеслав Калькаев <slavakalkaev@MacBook-Pro-Vaceslav.local>
2023-11-21 20:26:05 +0300

    добавил метод пиписька

--- src/GitTraining.java
+++ src/GitTraining.java
@@ -2,4 +2,7 @@ public class GitTraining {
     public void printing(){
         System.out.println("hello world");
     }
+    public  void pipisa(){
+        System.out.println("abracadabra");
+    }
 }
