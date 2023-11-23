50e5a0bebd1aca17e3fbb5870ef5dae999b459bc
DionisSerbin <reaganusm@gmail.com>
2023-11-23 20:26:15 +0300

    add stack interface to realization

---
+++ src/stack/Node.java
@@ -0,0 +1,14 @@
+package stack;
+
+public class Node {
+    /*
+     сами данные, те если у нас стек [4, 8, 9], то у ноды третьего элемента data = 9
+    */
+    private int data;
+
+    private Node next;
+
+    //конструктор
+
+    //геттеры / сеттеры
+}
---
+++ src/stack/StackImpl.java
@@ -0,0 +1,11 @@
+package stack;
+//тут будет реализация - начало положенио
+//public class StackImpl extends StackInterface{
+//    //верхушка стека
+//    Node top;
+//
+//    //размер стека
+//    int size;
+//}
+
+
