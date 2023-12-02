e7b384ec0c57d36af8f5c90e1316f3651186546e
Вячеслав Калькаев <slavakalkaev@MacBook-Pro-Vaceslav.local>
2023-12-03 01:30:47 +0300

    Сделал терминальное приложение по стеку

--- src/stack/Node.java
+++ src/stack/Node.java
@@ -1,14 +1,17 @@
 package stack;
 
 public class Node {
+
     /*
      сами данные, те если у нас стек [4, 8, 9], то у ноды третьего элемента data = 9
     */
-    private int data;
+    int data;
 
-    private Node next;
+    Node next;
 
     //конструктор
 
     //геттеры / сеттеры
+
+    //тут будет реализация - начало положенио
 }
