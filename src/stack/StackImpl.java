68d1f1bc2dcb2991463e180ce95cba222a3dc45d
Вячеслав Калькаев <slavakalkaev@MacBook-Pro-Vaceslav.local>
2023-12-07 00:46:57 +0300

    suchka

--- src/stack/StackImpl.java
+++ src/stack/StackImpl.java
@@ -15,7 +15,7 @@ public class StackImpl implements StackInterface {
 
     @Override
     public boolean isEmpty() {
-       return top == null;
+        return top == null;
     }
 
     @Override
@@ -25,9 +25,9 @@ public class StackImpl implements StackInterface {
 
     @Override
     public int push(int x) {
-        Node node = new Node(x,top);
-            top = node;
-            this.size ++;
+        Node node = new Node(x, top);
+        top = node;
+        this.size++;
         return x;
     }
 
@@ -35,10 +35,8 @@ public class StackImpl implements StackInterface {
     public int pop() {
         int currentHead = Integer.MIN_VALUE;
         if (isEmpty()) {
-            System.out.println("nothing added in stack"
-            );
-        }
-        else {
+            System.out.println("nothing added in stack");
+        } else {
             currentHead = top.getData();
             this.size = this.size - 1;
             this.top = this.top.getNext();
@@ -48,11 +46,11 @@ public class StackImpl implements StackInterface {
 
     @Override
     public int peek() {
-        int peekStack = Integer.MAX_VALUE;
+        int peekStack = Integer.MIN_VALUE;
         if (isEmpty()) {
             System.out.println("stack is empty");
-        }
-        else {peekStack = top.getData();
+        } else {
+            peekStack = top.getData();
         }
         return peekStack;
     }
@@ -62,32 +60,38 @@ class Popo {
     public static void main(String[] args) {
         StackImpl stack = new StackImpl();
         while (true) {
-            System.out.println("Варианты операций ");
+            System.out.println("Variants of operations ");
             System.out.println("1 - delete from stack");
             System.out.println("2 - add in stack");
             System.out.println("3 - stack is empty ?");
             System.out.println("4 - wanna know stack size");
             System.out.println("5 - wanna know head of stack");
-            System.out.println("Введите номер операции ");
+            System.out.println("Enter number of operations ");
             Scanner s = new Scanner(System.in);
             int num = s.nextInt();
-            if (num == 1){
-                System.out.println("delete from stack " + stack.pop());
+            if (num == 1) {
+                int b = stack.pop();
+                if (b != Integer.MIN_VALUE){
+                    System.out.println("delete from stack " + b);
+                }
             }
-            if (num == 2){
-                System.out.println("Write number you want to add");
+            if (num == 2) {
+                System.out.println("write number you want to add");
                 Scanner in = new Scanner(System.in);
                 int x = in.nextInt();
                 System.out.println("add in stack " + stack.push(x));
             }
-            if (num == 3){
-                    System.out.println("empty of stack is " + stack.isEmpty());
+            if (num == 3) {
+                System.out.println("empty of stack is " + stack.isEmpty());
             }
-            if (num == 4){
+            if (num == 4) {
                 System.out.println("size of stack is  " + stack.size());
             }
-            if (num == 5){
-                System.out.println("head of stack  " + stack.peek());
+            if (num == 5) {
+                int a = stack.peek();
+                if (a != Integer.MIN_VALUE){
+                    System.out.println("head of stack  " + a);
+                }
             }
         }
     }
